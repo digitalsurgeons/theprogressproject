06a795dcf79449ef96f06a163bb91442473e8e59
Adam Chambers <adammchambers@gmail.com>
2020-03-01 08:23:52 -0500

    Integrate HTML email template

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ require('dotenv').config()
 const express = require('express')
 const chart = require('./lib/chart')
 const mail = require('./lib/mail')
+const template = require('./lib/template')
 
 const app = express()
 app.use(express.json())
 
 app.post('/', (request, response) => ***REMOVED***
-  console.log('\nNew response')
-  console.log(request.body.form_response.answers)
-  chart(request.body.form_response.answers, mail)
+  const answers = request.body.form_response.answers
+
+  chart(answers, params => ***REMOVED***
+    mail(params, ***REMOVED***
+      to: 'ac@digitalsurgeons.com',
+      template
+    ***REMOVED***)
+  ***REMOVED***)
 ***REMOVED***)
 
 app.listen(3000, () => ***REMOVED***
