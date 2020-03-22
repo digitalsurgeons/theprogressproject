c57cc9014136322d34a29cd49ad5e3332759f8e5
Adam Chambers <adammchambers@gmail.com>
2020-03-22 16:30:25 -0400

    Change POST endpoint to /progress

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 app.use(express.static('public'))
 
-app.post('/', (request, response) => ***REMOVED***
+app.post('/progress', (request, response) => ***REMOVED***
   const answers = request.body.form_response.answers
 
   chart(answers, params => ***REMOVED***
