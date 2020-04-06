ffe215c64bd586d5b1dfbb9515897b40e7ad7a69
Adam Chambers <adammchambers@gmail.com>
2020-04-06 00:41:34 -0400

    Secure progress handler by checking Typeform payload

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 require('dotenv').config()
 
+const crypto = require('crypto')
 const express = require('express')
 const chart = require('./lib/chart')
 const mail = require('./lib/mail')
 const template = require('./lib/template')
 
 const app = express()
-app.use(express.json())
 
+app.use(
+  express.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf
+    },
+  })
+)
 app.use(express.static('public'))
 
 app.post('/progress', (req, res) => {
+  const expectedSig = req.header('Typeform-Signature')
+
+  const hash = crypto
+    .createHmac('sha256', process.env.TYPEFORM_SECRET)
+    .update(req.rawBody)
+    .digest('base64')
+
+  const actualSig = `sha256=${hash}`
+
+  if (actualSig !== expectedSig) {
+    res.status(403).send('invalid')
+    return
+  }
+
   const answers = req.body.form_response.answers
 
-  chart(answers, params => {
+  chart(answers, (params) => {
     mail(params, {
       bcc: process.env.EMAIL,
-      template
+      template,
     })
   })
 
@@ -25,7 +46,7 @@ app.post('/progress', (req, res) => {
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', {
-    root: './templates'
+    root: './templates',
   })
 })
 
