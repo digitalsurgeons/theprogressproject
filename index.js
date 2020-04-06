fc9bd030d0514ea0bac4154fc9274c402ba1f04c
Adam Chambers <adammchambers@gmail.com>
2020-04-06 00:41:34 -0400

    Secure progress handler by checking Typeform payload

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 require('dotenv').config()
 
+const crypto = require('crypto')
 const express = require('express')
 const chart = require('./lib/chart')
 const mail = require('./lib/mail')
 const template = require('./lib/template')
 
 const app = express()
-app.use(express.json())
 
+app.use(
+  express.json(***REMOVED***
+    verify: (req, res, buf) => ***REMOVED***
+      req.rawBody = buf
+    ***REMOVED***,
+  ***REMOVED***)
+)
 app.use(express.static('public'))
 
 app.post('/progress', (req, res) => ***REMOVED***
+  const expectedSig = req.header('Typeform-Signature')
+
+  const hash = crypto
+    .createHmac('sha256', process.env.TYPEFORM_SECRET)
+    .update(req.rawBody)
+    .digest('base64')
+
+  const actualSig = `sha256=$***REMOVED***hash***REMOVED***`
+
+  if (actualSig !== expectedSig) ***REMOVED***
+    res.status(403).send('invalid')
+    return
+  ***REMOVED***
+
   const answers = req.body.form_response.answers
 
-  chart(answers, params => ***REMOVED***
+  chart(answers, (params) => ***REMOVED***
     mail(params, ***REMOVED***
       bcc: process.env.EMAIL,
-      template
+      template,
     ***REMOVED***)
   ***REMOVED***)
 
@@ -25,7 +46,7 @@ app.post('/progress', (req, res) => ***REMOVED***
 
 app.get('/', (req, res) => ***REMOVED***
   res.sendFile('index.html', ***REMOVED***
-    root: './templates'
+    root: './templates',
   ***REMOVED***)
 ***REMOVED***)
 
